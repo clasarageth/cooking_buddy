76eaaabb7d1d55741de7ea2fa2d1926903a17be6
clasarageth <getheljayce.clasara@gmail.com>
2026-05-28 19:59:51 +0800

    update homeview

--- projectsite/recipes/views.py
+++ projectsite/recipes/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Recipe
 
 
 def home(request):
+
+    query = request.GET.get('q')
+
     recipes = Recipe.objects.all()
+
+    if query:
+        recipes = Recipe.objects.filter(
+            Q(title__icontains=query) |
+            Q(ingredients__icontains=query)
+        )
 
     context = {
         'recipes': recipes
