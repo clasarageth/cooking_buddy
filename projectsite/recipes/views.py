032531e7895175bc15ca2995ea1431042aac2c38
clasarageth <getheljayce.clasara@gmail.com>
2026-05-28 19:48:46 +0800

    add simple home view

--- projectsite/recipes/views.py
+++ projectsite/recipes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Recipe
 
-# Create your views here.
+
+def home(request):
+    recipes = Recipe.objects.all()
+
+    context = {
+        'recipes': recipes
+    }
+
+    return render(request, 'recipes/home.html', context)
