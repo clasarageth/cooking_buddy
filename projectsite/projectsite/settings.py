fe39aa12d91bb578cd145b7d8f87a38bd715a0ea
clasarageth <getheljayce.clasara@gmail.com>
2026-06-03 10:34:04 +0800

    fix login

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -125,3 +125,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = 'home'
